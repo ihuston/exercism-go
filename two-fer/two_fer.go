fda2fb788e0f1e14ed32481b5be93e2f486818d4
Ian Huston <ian@ianhuston.net>
2018-11-03 20:54:00 +0000

    Second exercise

---
+++ two-fer/two_fer.go
@@ -0,0 +1,12 @@
+// Package twofer provides the ShareWith function.
+package twofer
+
+import "fmt"
+
+// ShareWith returns a sharing string.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
