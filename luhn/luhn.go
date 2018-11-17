1cd3681b17327557a91135aae295383b187ed2d1
Ian Huston <ian@ianhuston.net>
2018-11-17 11:11:35 +0000

    Refactor Luhn algorithm for performance.

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,8 +1,8 @@
 // Package luhn provides calculation of the Luhn checksum.
 package luhn
 
-import "strconv"
 import "strings"
+import "unicode"
 
 // Valid checks whether a string contains a number valid per the Luhn algorithm.
 func Valid(input string) bool {
@@ -11,32 +11,23 @@ func Valid(input string) bool {
 		return false
 	}
 
-	_, err := strconv.ParseInt(cleaned, 10, 64)
-	if err != nil {
-		return false
-	}
-
-	evens := 0
-	odds := 0
-	for n := range cleaned {
-		index := len(cleaned) - n - 1
-		digit := cleaned[index]
-
-		value := int(digit) - int('0')
-		if n%2 == 0 {
-			evens += value
+	sum := 0
+	for n := len(cleaned) - 1; n >= 0; n-- {
+		char := rune(cleaned[n])
+		if !unicode.IsDigit(char) {
+			return false
+		}
+		value := int(char - '0')
+		if (len(cleaned)-n-1)%2 == 0 {
+			sum += value
 		} else {
 			value2 := value * 2
 			if value2 > 9 {
-				odds += value2 - 9
+				sum += value2 - 9
 			} else {
-				odds += value2
+				sum += value2
 			}
 		}
 	}
-
-	if (odds+evens)%10 == 0 {
-		return true
-	}
-	return false
+	return sum%10 == 0
 }
