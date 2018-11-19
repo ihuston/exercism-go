5e23121d70169c4a1b025ec38b04e130d36cce57
Ian Huston <ian@ianhuston.net>
2018-11-19 20:43:22 +0000

    First solution for reverse_string side exercise.

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,11 @@
+// Package reverse provides tools to reverse sequences.
+package reverse
+
+// String reverses a provided string.
+func String(s string) string{
+    r := []rune(s)
+    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+        r[i], r[j] = r[j], r[i]
+    }
+    return string(r)
+}
